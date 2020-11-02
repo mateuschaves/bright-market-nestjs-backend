5b6400e6bf2362d2723fc7673608609142da1e1b
Mateus Henrique <mateushenriquechaves1@gmail.com>
2020-11-02 12:27:00 -0300

    adding npx on scripts to deploy

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -5,12 +5,12 @@ import * as config from 'config';
 const dbConfig: any = config.get('db');
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
-    type: dbConfig.type,
-    host: dbConfig.host,
-    port: process.env.RDS_PORT || dbConfig.port,
-    username: process.env.RDS_USERNAME || dbConfig.username,
-    password: process.env.RDS_PASSWORD || dbConfig.password,
-    database: process.env.RDS_DB_NAME || dbConfig.database,
+    type: 'postgres',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || dbConfig.port,
+    username: process.env.DB_USERNAME || dbConfig.username,
+    password: process.env.DB_PASSWORD || dbConfig.password,
+    database: process.env.DB_NAME || dbConfig.database,
     entities: [__dirname + '/../**/*.entity.{js,ts}'],
     synchronize: process.env.TYPEORM_SYNC || dbConfig.synchronize,
 };
